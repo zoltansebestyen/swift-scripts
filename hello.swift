cf2594ba0ce17977f2920b6830a05d9fd0b5b3d0
Zoltan Sebestyen <zoltan.sebestyen@gmail.com>
2015-09-21 20:39:31 +0200

    hello.swift improved

--- hello.swift
+++ hello.swift
@@ -1,4 +1,18 @@
 #!/usr/bin/env swift
 print("hello world!!")
-dump(Process.arguments)
+// dump(Process.arguments)
+// print ( Process.arguments.count )
+if (Process.arguments.count < 2) {
+    let me = Process.arguments[0]
+        print("Usage: \(me) fileglob1 [fileglob2 ..]")
+            exit(-1)
+}
 
+for idx in 1...Process.arguments.count {
+    print Process.arguments[idx]
+}
+
+// if (Process.arguments.count > 1) {
+//     print(Process.arguments[1])
+// }
+// 
